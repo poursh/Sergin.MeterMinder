c4bd5cf4d9f4bb9f53ea446c9ceab2ec587fcb7f
Pejman Pourshirazi <pspejman@gmail.com>
2026-08-22 10:24:42 +0330

    Add Sergin.MeterMinder.DeviceManagement.Application.Contracts project shell

---
+++ src/Modules/DeviceManagement/Sergin.MeterMinder.DeviceManagement.Application.Contracts/DeviceManagementApplicationContractsAssemblyReference.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+namespace Sergin.MeterMinder.DeviceManagement.Application.Contracts;
+
+public static class DeviceManagementApplicationContractsAssemblyReference
+{
+    public static readonly Assembly Assembly = typeof(DeviceManagementApplicationContractsAssemblyReference).Assembly;
+}
